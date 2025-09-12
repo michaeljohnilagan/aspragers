08a682a33b6c1862a63048ed22d640aa814c433a
Michael Ilagan <michael.ilagan@mail.mcgill.ca>
2025-09-12 10:44:32 -0400

    implement ask random

--- cli.py
+++ cli.py
@@ -4,7 +4,9 @@ from datetime import datetime
 if __name__ == '__main__':
     # messages to the user
     welcome_message = '\n'+str(datetime.now())+\
-    ' | AspRAGers is ready! Enter your question, or EXIT to exit.'+'\n'
+    ' | AspRAGers is ready! Enter your question,'+\
+    ' RANDOM to draw a random question,'+\
+    ' or EXIT to exit.'+'\n'
     wait_message = '\n'+str(datetime.now())+\
     ' | AspRAGers is generating a response. Please wait...'+'\n'
     
@@ -12,15 +14,17 @@ if __name__ == '__main__':
     do_vector_search = rag.config['do_vector_search']
     num_results = rag.config['num_results']
     model_handle_llm = rag.config['model_handle_llm']
+    questions_filename = 'data/data-synth-question.csv'
 
     # take user input as query, then respond
     while True:
         query = input(welcome_message)
         if query.lower().strip() == 'exit':
             break
-        else:
-            print(wait_message)
-            print(rag.rag(query, \
-            do_vector_search=do_vector_search, num_results=num_results, \
-            model_handle_llm=model_handle_llm, seed=None))
-
+        if query.lower().strip() == 'random':
+            query = rag.get_random_question(questions_filename)
+            print(query)
+        print(wait_message)
+        print(rag.rag(query, \
+        do_vector_search=do_vector_search, num_results=num_results, \
+        model_handle_llm=model_handle_llm, seed=None))
